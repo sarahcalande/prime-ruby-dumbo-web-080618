f53bcdd11546a15a2e7a53c2d1f2f91f8aa987bc
scalande@yahoo.com <scalande@yahoo.com>
2018-07-22 14:03:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+
+def prime?(int)
+  return false if (int.even? && int > 2) || int <= 1
+
+  half_num = int / 2
+  x = 3
+  while x < half_num
+    return false if int % x == 0
+    x += 2
+  end
+
+  true
+end
