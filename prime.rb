3c61803f4f81b04b160d3427697d18986ce58bfa
scalande@yahoo.com <scalande@yahoo.com>
2018-07-22 14:02:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,3 +2,14 @@
 
 
 def prime?(int)
+  return false if (int.even? && int > 2) || number <= 1
+
+  half_num = number / 2
+  x = 3
+  while x < half_num
+    return false if number % x == 0
+    x += 2
+  end
+
+  true
+end
